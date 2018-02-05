e872d29980c09cdc9871382e9bad44bd00267a93
drie martin <martin@drie.co>
2018-02-05 07:51:58 +0100

    added closeItemDetailsModal()

--- track-spending/js/app.js
+++ track-spending/js/app.js
@@ -139,11 +139,16 @@ function deleteCurrentItem() {
   localforage.removeItem(currentItem).then(function() {
     // Run this code once the key has been removed.
     alert("Item deleted!");
-    document.getElementById("item-modal").style.display = "none";
-}).catch(function(err) {
-    // This code runs if there were any errors
-    console.log(err);
-});
+    closeItemDetailsModal();
+    showLast10Entries();
+  }).catch(function(err) {
+      // This code runs if there were any errors
+      console.log(err);
+  });
+}
+
+function closeItemDetailsModal() {
+  document.getElementById("item-modal").style.display = "none";
 }
 
 var currentItem = "";
